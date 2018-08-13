87ce53e3b8d8305ac4e27bde1f5104f880127ad9
Azrul Haifan <ahaifan@gmail.com>
2018-08-13 16:35:54 +0700

    fix C_INCLUDES must be under the source or output directories

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -78,15 +78,15 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/util \
         $(LOCAL_PATH)/HAL3 \
         hardware/libhardware/include/hardware \
-        hardware/qcom/media/msm8996/libstagefrighthw \
-        hardware/qcom/media/msm8996/mm-core/inc \
+        hardware/qcom/media/msm8998/libstagefrighthw \
+        hardware/qcom/media/msm8998/mm-core/include \
         system/core/include/cutils \
         system/core/include/system \
         system/media/camera/include/system
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
-        hardware/qcom/camera/msm8998/QCamera2/HAL
+        $(LOCAL_PATH)/QCamera2/HAL
 
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
@@ -108,7 +108,7 @@ endif
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
-        hardware/qcom/display/msm8996/libqservice
+        hardware/qcom/display/msm8998/libqservice
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
