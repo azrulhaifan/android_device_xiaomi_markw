6447a77f42d371e113f14795ff1c74a05c48c7ca
Azrul Haifan <ahaifan@gmail.com>
2018-08-13 16:52:54 +0700

    fix C_INCLUDES must be under the source or output directories

--- camera/QCamera2/stack/mm-jpeg-interface/test/Android.mk
+++ camera/QCamera2/stack/mm-jpeg-interface/test/Android.mk
@@ -25,9 +25,9 @@ LOCAL_C_INCLUDES := $(MM_JPEG_TEST_PATH)
 LOCAL_C_INCLUDES += $(MM_JPEG_TEST_PATH)/../inc
 LOCAL_C_INCLUDES += $(MM_JPEG_TEST_PATH)/../../common
 LOCAL_C_INCLUDES += $(MM_JPEG_TEST_PATH)/../../mm-camera-interface/inc
-LOCAL_C_INCLUDES += $(OMX_HEADER_DIR)
-LOCAL_C_INCLUDES += $(OMX_CORE_DIR)/qexif
-LOCAL_C_INCLUDES += $(OMX_CORE_DIR)/qomx_core
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../.. \
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../mm-image-codec/qexif \
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../mm-image-codec/qomx_core
 
 LOCAL_C_INCLUDES+= $(kernel_includes)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
