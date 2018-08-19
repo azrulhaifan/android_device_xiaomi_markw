8f29d60808f53f63bb2a5de97244af7fe75d4c00
Azrul Haifan <ahaifan@gmail.com>
2018-08-19 10:47:34 +0700

    remove RRO

--- device.mk
+++ device.mk
@@ -16,17 +16,13 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-#$(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
+$(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 $(call inherit-product, vendor/xiaomi/markw/markw-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    Bluetooth \
-    framework-res
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
