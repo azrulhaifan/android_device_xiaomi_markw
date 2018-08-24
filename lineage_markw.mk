2d058c2d5ad4a8c41f8f2da7619db18d8c4573e6
Azrul Haifan <ahaifan@gmail.com>
2018-08-24 14:13:30 +0700

    new LOS 16

--- lineage_markw.mk
+++ lineage_markw.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/xiaomi/markw/full_markw.mk)
 
-# Inherit some common Potato stuff.
-$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := potato_markw
+PRODUCT_NAME := lineage_markw
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo potato_markw-eng
+add_lunch_combo lineage_markw-eng
