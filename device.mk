96c0262bd463391f2f9fc4908e77149befae3eae
Azrul Haifan <ahaifan@gmail.com>
2018-08-26 06:10:46 +0700

    remove device spesific vndk-sp

--- device.mk
+++ device.mk
@@ -356,10 +356,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
-# VNDK-SP:
-PRODUCT_PACKAGES += \
-    vndk-sp
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
