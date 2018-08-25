c0ceefa22f9cc0cf89ec52037387dd80d75a186f
Azrul Haifan <ahaifan@gmail.com>
2018-08-25 20:02:18 +0700

    Use passthrough keymaster hal

--- device.mk
+++ device.mk
@@ -242,8 +242,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-impl
 
 # Lights
 PRODUCT_PACKAGES += \
