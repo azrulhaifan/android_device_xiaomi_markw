e97a999a1fe392a83f3a8a353e6211ac0ba8d023
Azrul Haifan <ahaifan@gmail.com>
2018-08-25 20:37:27 +0700

    update fingerprint

--- biometrics/Android.mk
+++ biometrics/Android.mk
@@ -5,14 +5,6 @@ LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.0-service.xiaomi_markw
 LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.0-service.xiaomi_markw.rc
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SRC_FILES := \
-    BiometricsFingerprint.cpp \
-    service.cpp \
-    fingerprintd/FingerprintDaemonCallbackProxy.cpp \
-    fingerprintd/FingerprintDaemonProxy.cpp \
-    fingerprintd/IFingerprintDaemonCallback.cpp \
-    fingerprintd/IFingerprintDaemon.cpp \
-    fingerprintd/wrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libbinder \
@@ -27,5 +19,3 @@ LOCAL_SHARED_LIBRARIES := \
     android.hardware.biometrics.fingerprint@2.1 \
 
 include $(BUILD_EXECUTABLE)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
