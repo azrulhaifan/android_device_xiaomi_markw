2fa6f0093ed63209b4c593800ef96c4b023e7f40
Azrul Haifan <ahaifan@gmail.com>
2018-08-27 19:45:19 +0700

    fix 2

--- havoc_markw.mk
+++ havoc_markw.mk
@@ -14,13 +14,25 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/xiaomi/markw/full_markw.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Havoc stuff.
 $(call inherit-product, vendor/havoc/products/common.mk)
 
+# Inherit from markw device
+$(call inherit-product, device/xiaomi/markw/device.mk)
+
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := havoc_markw
+PRODUCT_DEVICE := markw
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 4 Prime
+PRODUCT_MANUFACTURER := Xiaomi
+
 BOARD_VENDOR := Xiaomi
+TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
