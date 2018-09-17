662cc7ed1fdb7e1c2a3804474f53ad9ff92ffc5f
azrulhaifan <ahaifan@gmail.com>
2018-09-18 02:55:49 +0700

    PE-P Bringup

--- aosp_markw.mk
+++ aosp_markw.mk
@@ -21,15 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from markw device
 $(call inherit-product, device/xiaomi/markw/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PE stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4 Prime
 PRODUCT_DEVICE := markw
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_markw
+PRODUCT_NAME := aosp_markw
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
